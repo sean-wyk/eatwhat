63c8df65a4e9721d3dda9f744445b672f2d1a471
wyksean3310 <128277471+sean-wyk@users.noreply.github.com>
2024-10-31 21:15:18 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,17 +1,33 @@
+// worker.js
+
+// 存储食物列表
 let foods = [];
 
 // 监听来自主线程的消息
 onmessage = function(e) {
     const { type, data } = e.data;
-    if (type === 'loadFoods') {
-        foods = data;
-        postMessage({ type: 'foodsLoaded', data: foods });
-    } else if (type === 'getRandomFood') {
-        if (foods.length > 0) {
-            const randomFood = foods[Math.floor(Math.random() * foods.length)];
-            postMessage({ type: 'randomFood', data: randomFood });
-        } else {
-            postMessage({ type: 'randomFood', data: '食物列表为空' });
-        }
+
+    switch(type) {
+        case 'loadFoods':
+            // 接收并存储食物列表
+            foods = data;
+            // 通知主线程食物列表已加载，发送食物数量作为反馈
+            postMessage({ type: 'foodsLoaded', data: foods.length });
+            break;
+
+        case 'getRandomFood':
+            // 返回随机食物
+            if (foods.length > 0) {
+                const randomIndex = Math.floor(Math.random() * foods.length);
+                const randomFood = foods[randomIndex];
+                postMessage({ type: 'randomFood', data: randomFood });
+            } else {
+                postMessage({ type: 'randomFood', data: '食物列表为空' });
+            }
+            break;
+
+        default:
+            // 处理未知类型的消息
+            console.warn(`未知的消息类型: ${type}`);
     }
 };
